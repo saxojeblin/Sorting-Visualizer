3b53d36f72e28e1557f11b4e14e8f8b1c35c40a9
Nick Rubio <nrubio327@gmail.com>
2022-05-09 17:46:56 -0400

    added calls to each sorting function and managed memory

--- main.cpp
+++ main.cpp
@@ -21,8 +21,23 @@ void run_sorts() {
 
     //Create a temperary array that will be used in the sorts
     int *temp_numbers = (int*) malloc(sizeof(int) * 50);
+    
+    //Perform each sort
+    //Before each sort, we make sure we are using the originial, unsorted list, hence the memcpy
+    memcpy(temp_numbers, unsorted_numbers, sizeof(int) * 50);
+    bubble_sort(temp_numbers);
 
-    //Sorts go here    
+    memcpy(temp_numbers, unsorted_numbers, sizeof(int) * 50);
+    selection_sort(temp_numbers);
+
+    memcpy(temp_numbers, unsorted_numbers, sizeof(int) * 50);
+    insertion_sort(temp_numbers);
+
+    memcpy(temp_numbers, unsorted_numbers, sizeof(int) * 50);
+    quick_sort(temp_numbers);
+
+    memcpy(temp_numbers, unsorted_numbers, sizeof(int) * 50);
+    merge_sort(temp_numbers);
 
     //Free our allocated arrays of numbers
     free(unsorted_numbers);
